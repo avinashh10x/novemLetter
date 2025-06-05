481043b53780270c656e8e3031dc9f3446a6bbcf
Avinash <raavaannn@gmail.com>
2025-06-05 14:10:07 +0530

    Update API_URL to use environment variable for better configuration

--- fronntend/src/services/LetterServices.jsx
+++ fronntend/src/services/LetterServices.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
-// const API_URL = "https://maqusad.onrender.com/api";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/`;
 
 //  Get all Letters
 const getAllLetters = async (page = 1, limit = 5) => {
